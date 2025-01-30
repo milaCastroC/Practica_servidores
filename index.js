77c1763208d4275d909f3b1b451e97f8006fe541
Maria Camila Castro <castro.maria.5191@eam.edu.co>
2025-01-30 16:11:14 -0500

    EJERCICIOS 11 AL 20

--- index.js
+++ index.js
@@ -140,6 +140,140 @@ app.get('/libro/:libro', function (req, res) {
     });
 });
 
+//EJERCICIO 11
+app.post('/departamento', function (req, res) {
+    let departamento = req.query.departamento;
+    let cabecera = req.header("Content-Type");
+    let nombre = req.body.nombre;
+    let puesto = req.body.puesto;
+    let salario = req.body.salario;
+    return res.status(200).json({
+        "Status": "Registro de empleado",
+         cabecera: cabecera,
+         departamento: departamento,
+         nombre: nombre,
+         puesto: puesto,
+         salario: salario
+    });
+});
+
+//EJERCICIO 12
+app.get('/vehiculo/:vehiculo', function (req, res) {
+    let vehiculo = req.params.vehiculo;
+    let color = req.query.color;
+    return res.status(200).json({
+        "Status": "Consulta de vehículo",
+         vehiculo: vehiculo,
+         color: color
+    });
+});
+
+//EJERCICIO 13
+app.post('/compra', function (req, res) {
+    let cabecera = req.header("Content-Type");
+    let idCliente = req.header("X-Client-ID");
+    let numero = req.body.numero;
+    let monto = req.body.monto;
+    let fecha = req.body.fecha;
+    return res.status(200).json({
+        "Status": "Compra realizada",
+         cabecera: cabecera,
+         idCliente: idCliente,
+         numero: numero,
+         monto: monto,
+         fecha: fecha
+    });
+});
+
+//EJERCICIO 14
+app.get('/pelicula/:pelicula', function (req, res) {
+    let pelicula = req.params.pelicula;
+    return res.status(200).json({
+        "Status": "Consulta de película",
+         pelicula: pelicula
+    });
+});
+
+//EJERCICIO 15
+app.post('/clase', function (req, res) {
+    let clase = req.query.clase;
+    let cabecera = req.header("Content-Type");
+    let numero_vuelo = req.body.numero_vuelo;
+    let origen = req.body.origen;
+    let destino = req.body.destino;
+    return res.status(200).json({
+        "Status": "Reserva de vuelo",
+         cabecera: cabecera,
+         clase: clase,
+         numero_vuelo: numero_vuelo,
+         origen: origen,
+         destino: destino
+    });
+});
+
+//EJERCICIO 16
+app.get('/suscripcion/:suscripcion', function (req, res) {
+    let suscripcion = req.params.suscripcion;
+    let idUsuario = req.header("X-User-ID");
+    return res.status(200).json({
+        "Status": "Consulta de suscripción",
+         suscripcion: suscripcion,
+         idUsuario: idUsuario
+    });
+});
+
+//EJERCICIO 17
+app.post('/moneda', function (req, res) {
+    let cabecera = req.header("Content-Type");
+    let monto = req.body.monto;
+    let fecha = req.body.fecha;
+    let destinatario = req.body.destinatario;
+    return res.status(200).json({
+        "Status": "Consignacion realizada",
+        cabecera: cabecera,
+        monto: monto,
+        fecha: fecha,
+        destinatario: destinatario
+    });
+});
+
+//EJERCICIO 18
+app.get('/ticket/:ticket', function (req, res) {
+    let ticket = req.params.ticket;
+    return res.status(200).json({
+        "Status": "Consulta de ticket",
+         ticket: ticket,
+    });
+});
+
+//EJERCICIO 19
+app.post('/libreria', function (req, res) {
+    let cabecera = req.header("Content-Type");
+    let idLibreria = req.header("X-Library-ID");
+    let titulo = req.body.titulo;
+    let autor = req.body.autor;
+    let año = req.body.año;
+    return res.status(200).json({
+        "Status": "Compra de libro",
+         cabecera: cabecera,
+         idLibreria: idLibreria,
+         titulo: titulo,
+         autor: autor,
+         año: año
+    });
+});
+
+//EJERCICIO 20
+app.get('/evento/:evento', function (req, res) {
+    let evento = req.params.evento;
+    let fecha = req.query.fecha;
+    return res.status(200).json({
+        "Status": "Consulta de evento",
+         evento: evento,
+         fecha: fecha
+    });
+});
+
 app.listen(10101, function () { 
     console.log('Example app listening on port 10101!'); 
 });
